177581c7f9e80698ed3cb60ea2e4f32a1cc5b4da
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-05-13 23:50:41 +0200

    new type of tests v11

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,12 +8,10 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class CalculatorTest {
     @Test
-    public void testAdd() {
-        String param1 = System.getProperty("param1");
-        String param2 = System.getProperty("param2");
-        String param3 = System.getProperty("param3");
+    public void testAdd(int param1, int param 2, int param3) {
         Calculator calculator = new Calculator();
-        int result = calculator.add(Integer.parseInt(param1), Integer.parseInt(param2));
-        Assertions.assertEquals(Integer.parseInt(param3), result, "Expected result is " + param3);
+        int result = calculator.add(param1, param2);
+        Assertions.assertEquals(param3, result, "Expected result is " + param3);
+    }
     }
 }
