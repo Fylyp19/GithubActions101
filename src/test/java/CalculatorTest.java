e06e109b1a220faf259cccac25cb3b1b1bff013c
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-05-13 23:30:55 +0200

    new type of tests v3

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,17 +16,4 @@ public class CalculatorTest {
         int result = calculator.add(Integer.parseInt(param1), Integer.parseInt(param2));
         Assertions.assertEquals(Integer.parseInt(param3), result, "Expected result is " + param3);
     }
-    /*@Test
-    public void testSub() {
-        Calculator calculator = new Calculator();
-        int result = calculator.sub(Integer.parseInt(System.getenv("NUM1")), Integer.parseInt(System.getenv("NUM2")));
-        Assertions.assertEquals(Integer.parseInt(System.getenv("RESULT")), result, "Expected result is " + System.getenv("RESULT"));
-    }
-    @Test
-    public void testMultiply() {
-        Calculator calculator = new Calculator();
-        int result = calculator.multiply(Integer.parseInt(System.getenv("NUM1")), Integer.parseInt(System.getenv("NUM2")));
-        Assertions.assertEquals(Integer.parseInt(System.getenv("RESULT")), result, "Expected result is " + System.getenv("RESULT"));
-    }
-     */
 }
