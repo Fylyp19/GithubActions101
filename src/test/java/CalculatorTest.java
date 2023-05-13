2d6f2dd43c6da515b026502371e4ee4f6ce16683
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-05-14 00:02:37 +0200

    new type of tests v18

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,9 +11,9 @@ import java.util.stream.Stream;
 public class CalculatorTest {
     @Test
     public void testAdd() {
-        String param1 = System.getProperty("param1");
-        String param2 = System.getProperty("param2");
-        String param3 = System.getProperty("param3");
+        String param1 = System.getenv("param1");
+        String param2 = System.getenv("param2");
+        String param3 = System.getenv("param3");
         Calculator calculator = new Calculator();
         int result = calculator.add(Integer.parseInt(param1), Integer.parseInt(param2));
         Assertions.assertEquals(Integer.parseInt(param3), result, "Expected result is " + param3);
