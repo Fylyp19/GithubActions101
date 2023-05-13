9018efb2f0e0e345c75d637f9595b482f3a82f8e
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-05-13 23:25:44 +0200

    new type of tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,15 +3,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+
+
 @RunWith(JUnit4.class)
 public class CalculatorTest {
     @Test
     public void testAdd() {
+        String param1 = System.getProperty("param1");
+        String param2 = System.getProperty("param2");
+        String param3 = System.getProperty("param3");
         Calculator calculator = new Calculator();
-        int result = calculator.add(Integer.parseInt(System.getenv("NUM1")), Integer.parseInt(System.getenv("NUM2")));
-        Assertions.assertEquals(Integer.parseInt(System.getenv("RESULT")), result, "Expected result is " + System.getenv("RESULT"));
+        int result = calculator.add(Integer.parseInt(param1), Integer.parseInt(param2));
+        Assertions.assertEquals(Integer.parseInt(param3), result, "Expected result is " + param3));
     }
-    @Test
+    /*@Test
     public void testSub() {
         Calculator calculator = new Calculator();
         int result = calculator.sub(Integer.parseInt(System.getenv("NUM1")), Integer.parseInt(System.getenv("NUM2")));
@@ -23,4 +28,5 @@ public class CalculatorTest {
         int result = calculator.multiply(Integer.parseInt(System.getenv("NUM1")), Integer.parseInt(System.getenv("NUM2")));
         Assertions.assertEquals(Integer.parseInt(System.getenv("RESULT")), result, "Expected result is " + System.getenv("RESULT"));
     }
+     */
 }
